85f27cc7a8e8ea098b10bdf68618298f204b182e
bruno134 <brunodn@gmail.com>
2021-08-22 20:25:09 -0300

    Adição de JWT e configurações no yml

---
+++ src/main/java/com/bdnrfob/auth/jwt/AuthenticationProviderUserPassword.java
@@ -0,0 +1,41 @@
+package com.bdnrfob.auth.jwt;
+
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.security.authentication.*;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Singleton;
+import java.util.ArrayList;
+
+@Singleton
+public class AuthenticationProviderUserPassword implements AuthenticationProvider {
+
+    private final static Logger LOG = LoggerFactory.getLogger(AuthenticationProviderUserPassword.class);
+
+    @Override
+    public Publisher<AuthenticationResponse> authenticate(
+            @Nullable final HttpRequest<?> httpRequest,
+            final AuthenticationRequest<?, ?> authenticationRequest) {
+        return Flowable.create(emitter -> {
+            final Object identity = authenticationRequest.getIdentity();
+            final Object password = authenticationRequest.getSecret();
+
+            LOG.debug("User {} tries to login... ", identity);
+
+            if(identity.equals("my-user") && password.equals("secret")){
+                //pass
+                emitter.onNext(new UserDetails((String) identity,new ArrayList<>()));
+                emitter.onComplete();
+                return;
+            }
+
+            emitter.onError(new AuthenticationException(new AuthenticationFailed("Wrong username or password")));
+
+        }, BackpressureStrategy.ERROR);
+    }
+}
--- src/main/java/com/bdnrfob/broker/MarketsController.java
+++ src/main/java/com/bdnrfob/broker/MarketsController.java
@@ -4,10 +4,12 @@ import com.bdnrfob.broker.model.Symbol;
 import com.bdnrfob.broker.store.InMemoryStore;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 
-import java.util.ArrayList;
 import java.util.List;
 
+@Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/markets")
 public class MarketsController {
 
